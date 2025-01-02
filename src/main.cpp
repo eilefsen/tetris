da7795f0c8c525741bbc06cf32a2426eff1b9c4f
Emma Eilefsen Glenna <emma@eilefsen.net>
2025-01-02 16:59:25 +0100

    collisions

--- src/main.cpp
+++ src/main.cpp
@@ -22,148 +22,142 @@ struct Block {
 // Tetramino constants use relative block coordinates
 class Tetramino {
   private:
-	Coordinate orig1;
-	Coordinate orig2;
-	Coordinate orig3;
-	Coordinate orig4;
-	int fall_count = 0;
-	int right_count = 0;
-	bool rotated = false;
+	Coordinate orig[4];
+	int pattern[4][4];
 
   public:
-	Block one;
-	Block two;
-	Block three;
-	Block four;
+	Block blocks[4];
 
 	void fall() {
-		one.pos.y += 1;
-		two.pos.y += 1;
-		three.pos.y += 1;
-		four.pos.y += 1;
-		++fall_count;
+		for (auto &b : blocks) {
+			b.pos.y += 1;
+		}
 	}
 	void left() {
-		one.pos.x -= 1;
-		two.pos.x -= 1;
-		three.pos.x -= 1;
-		four.pos.x -= 1;
-		--right_count;
+		for (auto &b : blocks) {
+			b.pos.x -= 1;
+		}
 	}
 	void right() {
-		one.pos.x += 1;
-		two.pos.x += 1;
-		three.pos.x += 1;
-		four.pos.x += 1;
-		++right_count;
+		for (auto &b : blocks) {
+			b.pos.x += 1;
+		}
 	}
 
 	void rotate() {
-		int x1 = orig1.x;
-		int x2 = orig2.x;
-		int x3 = orig3.x;
-		int x4 = orig4.x;
+		for (int i = 0; i < 4; ++i) {
+			int x_offset = blocks[i].pos.x - orig[i].x;
+			int y_offset = blocks[i].pos.y - orig[i].y;
 
-		if (!rotated) {
-			orig1.x = orig1.y;
-			orig2.x = orig2.y;
-			orig3.x = orig3.y;
-			orig4.x = orig4.y;
-			orig1.y = -x1;
-			orig2.y = -x2;
-			orig3.y = -x3;
-			orig4.y = -x4;
-			rotated = true;
-		} else {
-			orig1.x = -orig1.y;
-			orig2.x = -orig2.y;
-			orig3.x = -orig3.y;
-			orig4.x = -orig4.y;
-			orig1.y = x1;
-			orig2.y = x2;
-			orig3.y = x3;
-			orig4.y = x4;
-			rotated = false;
+			// TODO: Refine rotation
+			Coordinate new_pos{.x = orig[i].y + 1, .y = -orig[i].x};
+
+			blocks[i].pos.x = new_pos.x + x_offset;
+			blocks[i].pos.y = new_pos.y + y_offset;
+			orig[i].x = new_pos.x;
+			orig[i].y = new_pos.y;
 		}
-
-		one.pos.x = orig1.x + right_count;
-		two.pos.x = orig2.x + right_count;
-		three.pos.x = orig3.x + right_count;
-		four.pos.x = orig4.x + right_count;
-
-		one.pos.y = orig1.y + fall_count;
-		two.pos.y = orig2.y + fall_count;
-		three.pos.y = orig3.y + fall_count;
-		four.pos.y = orig4.y + fall_count;
 	}
 
-	int lowest_y() {
-		int a = std::max(one.pos.y, two.pos.y);
-		int b = std::max(three.pos.y, four.pos.y);
-		return std::max(a, b);
-	}
+	Tetramino clone() const { return Tetramino(*this); }
 
-	Tetramino *clone() const { return new Tetramino(*this); }
-
-	Tetramino(
-		Color color, Coordinate pos1, Coordinate pos2, Coordinate pos3, Coordinate pos4
-	)
-		: one(Block{pos1, .color = color}), two(Block{pos2, .color = color}),
-		  three(Block{pos3, .color = color}), four(Block{pos4, .color = color}),
-		  orig1(pos1), orig2(pos2), orig3(pos3), orig4(pos4) {}
+	Tetramino(Color color, Coordinate positions[4])
+		: blocks{Block{positions[0], .color = color}, Block{positions[1], .color = color}, Block{positions[2], .color = color}, Block{positions[3], .color = color}},
+		  orig{positions[0], positions[1], positions[2], positions[3]} {}
 };
 
-static const Tetramino *I_TET = new Tetramino(
-	SKYBLUE,
-	{
-		.x = 0,
-		.y = 0,
-	},
-	{
-		.x = 1,
-		.y = 0,
-	},
-	{
-		.x = 2,
-		.y = 0,
-	},
-	{
-		.x = 3,
-		.y = 0,
-	}
-);
-static const Tetramino *T_TET = new Tetramino(
-	PURPLE,
-	{
-		.x = 0,
-		.y = 0,
-	},
-	{
-		.x = 1,
-		.y = 0,
-	},
-	{
-		.x = 2,
-		.y = 0,
-	},
-	{
-		.x = 1,
-		.y = 1,
-	}
-);
+struct Collision {
+	bool up = false;
+	bool down = false;
+	bool left = false;
+	bool right = false;
+};
+Collision check_collision(Block tet[4], std::vector<Block> board) {
+	Collision col;
+	int lowest_y = 0;
+	int rightest_x = 0;
+	int leftest_x = GRID_HEIGHT - 1;
 
-void move(Tetramino *t) {
+	for (int i = 0; i < 4; ++i) {
+		Coordinate t = tet[i].pos;
+
+		if (t.y > lowest_y) {
+			lowest_y = t.y;
+		}
+		if (t.x > rightest_x) {
+			rightest_x = t.x;
+		}
+		if (t.x < leftest_x) {
+			leftest_x = t.x;
+		}
+
+		for (const auto &b : board) {
+			if (t.x == b.pos.x) {
+				if (t.y == b.pos.y - 1) {
+					col.down = true;
+					break;
+				}
+				if (t.y == b.pos.y + 1) {
+					col.up = true;
+					break;
+				}
+			} else if (t.y == b.pos.y) {
+				if (t.x == b.pos.x - 1) {
+					col.right = true;
+				}
+				if (t.x == b.pos.x + 1) {
+					col.left = true;
+				}
+			}
+		}
+	}
+
+	if (lowest_y == GRID_HEIGHT - 1) {
+		col.down = true;
+	}
+	if (rightest_x == GRID_WIDTH - 1) {
+		col.right = true;
+	}
+	if (leftest_x == 0) {
+		col.left = true;
+	}
+
+	return col;
+}
+
+Tetramino create_i_tet() {
+	Coordinate pos[4]{
+		{.x = 0, .y = 0},
+		{.x = 1, .y = 0},
+		{.x = 2, .y = 0},
+		{.x = 3, .y = 0},
+	};
+	return Tetramino(SKYBLUE, pos);
+}
+
+Tetramino create_t_tet() {
+	Coordinate pos[4]{
+		{.x = 0, .y = 0},
+		{.x = 1, .y = 0},
+		{.x = 2, .y = 0},
+		{.x = 1, .y = 1},
+	};
+	return Tetramino(PURPLE, pos);
+}
+
+void move(Tetramino *t, Collision c) {
 	// TODO: add key hold
-	if (IsKeyPressed(KEY_H)) {
+
+	if (IsKeyPressed(KEY_H) && !c.left) {
 		t->left();
 	}
-	// TODO: add key hold
-	if (IsKeyPressed(KEY_L)) {
+	if (IsKeyPressed(KEY_L) && !c.right) {
 		t->right();
 	}
-	if (IsKeyPressed(KEY_J)) {
+	if (IsKeyPressed(KEY_J) && !c.down) {
 		t->fall();
 	}
+	// TODO: Write collision logic for rotate
 	if (IsKeyPressed(KEY_R)) {
 		t->rotate();
 	}
@@ -173,31 +167,37 @@ void draw_blocks(std::vector<Block> blocks);
 
 int main() {
 	// init
+	SetTraceLogLevel(LOG_ALL);
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tetris!");
 	SetTargetFPS(FPS_TARGET);
 
 	std::vector<Block> blocks{};
-	Tetramino *tet = I_TET->clone();
+	Tetramino tet = create_i_tet();
 
 	int game_time = 0;
+	Collision col{};
 
 	// run
 	while (!WindowShouldClose()) {
 		// update
 		std::vector<Block> total_blocks = blocks;
-		total_blocks.push_back(tet->one);
-		total_blocks.push_back(tet->two);
-		total_blocks.push_back(tet->three);
-		total_blocks.push_back(tet->four);
+		total_blocks.insert(
+			total_blocks.begin(), std::begin(tet.blocks), std::end(tet.blocks)
+		);
 
 		if (game_time % FPS_TARGET == 0) {
-			tet->fall();
+			tet.fall();
+			game_time = 0;
 		}
-		move(tet);
-		if (tet->lowest_y() == GRID_HEIGHT) {
+
+		printf("%d%d%d\n", col.left, col.down, col.right);
+		if (col.down) {
 			blocks = total_blocks;
-			tet = T_TET->clone();
+			tet = create_t_tet();
+		} else {
+			move(&tet, col);
 		}
+		col = check_collision(tet.blocks, blocks);
 
 		// draw
 		BeginDrawing();
