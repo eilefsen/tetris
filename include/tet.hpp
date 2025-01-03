6e5ec71dd089726a4455a1f5dd1d91352b7c77c8
Emma Eilefsen Glenna <emma@eilefsen.net>
2025-01-03 23:32:21 +0100

    split Tetramino stuff into its own file and header

---
+++ include/tet.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cstdint>
+#include <raylib.h>
+
+struct Coordinate {
+	int x; // X coordinate in grid
+	int y; // Y coordinate in grid
+};
+
+struct Block {
+	Coordinate pos;
+	Color color;
+};
+// Tetramino constants use relative block coordinates
+class Tetramino {
+  private:
+	uint16_t pattern[4]; // Binary numbers each representing a 4x4 grid rotation
+	int x_offset = 3;
+	int y_offset = -1;
+	int pattern_idx = 0;
+	// `pattern` should only contain 4 ones, will otherwise return early, and log error.
+	void create_blocks(uint16_t pattern, Color color);
+
+  public:
+	Block blocks[4];
+
+	void fall();
+	void left();
+	void right();
+
+	void rotate();
+
+	Tetramino(Color color, uint16_t pattern[4]);
+};
+
+Tetramino create_i_tet();
+Tetramino create_t_tet();
+Tetramino create_j_tet();
+Tetramino create_l_tet();
+Tetramino create_o_tet();
+Tetramino create_s_tet();
+Tetramino create_z_tet();
+Tetramino create_random_tet();
